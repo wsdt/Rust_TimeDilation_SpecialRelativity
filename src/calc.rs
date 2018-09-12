ee3df95863cdafeb72195055d287ce0ba9813ba6
kevin <kevin.riedl.privat@gmail.com>
2018-09-12 03:15:57 +0200

    added test execution, works!

--- src/calc.rs
+++ src/calc.rs
@@ -6,9 +6,9 @@ Observer time = (proper time)/(sqrt(1-(velocity/speed_of_light)^2))
 pub mod time_dilation_module {
     /** SPEED_OF_LIGHT often used as 'c' in physics/mathematical formulas.
         Following speed is true for light when travelling inside a vacuum. */
-    const SPEED_OF_LIGHT: f32 = 299792.458; //in km/s
+    const SPEED_OF_LIGHT: f64 = 299792.458; //in km/s
 
     pub fn calc_time_dilation(proper_time: f64, velocity: f64) -> f64 {
-        (proper_time) / ((1 - (velocity / SPEED_OF_LIGHT).powi(2)).sqrt())
+        (proper_time) / ((1.0 - (velocity / SPEED_OF_LIGHT).powi(2)).sqrt())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,7 @@ fn print_main_menu() {
     println!("~ © Riedl Kevin, 2018 ~");
     println!("\nWelcome and thank you for testing/using my time dilation calculator.\n\
         As far as I know, it's the first one which has been written in Rust!");
-    
+
+    //Example execution (works)
+    println!("Result: {}",time_dilation_module::calc_time_dilation(1.0,299792.457));
 }
