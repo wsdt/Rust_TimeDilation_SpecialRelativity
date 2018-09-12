c971709bd7771a670f30832bb9f07965bb75036b
kevin <kevin.riedl.privat@gmail.com>
2018-09-12 02:27:36 +0200

    added formula and constant

---
+++ src/calc.rs
@@ -0,0 +1,11 @@
+/** Special relativity, time-dilation formula:
+
+Observer time = (proper time)/(sqrt(1-(velocity/speed_of_light)^2))
+*/
+
+/** SPEED_OF_LIGHT often used as 'c' in physics/mathematical formulas.
+Following speed is true for light when travelling inside a vacuum. */
+const SPEED_OF_LIGHT: f32 = 299792.458; //in km/s
+
+
+
