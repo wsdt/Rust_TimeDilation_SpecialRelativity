a4862d34a037012988fd987cd2f8ba15d72d73cb
kevin <kevin.riedl.privat@gmail.com>
2018-09-12 02:10:56 +0200

    added main menu welcome msg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    print_main_menu();
+}
+
+fn print_main_menu() {
+    println!("********** Rust - Time Dilation Calculator *************");
+    println!("~ © Riedl Kevin, 2018 ~");
+    println!("\nWelcome and thank you for testing/using my time dilation calculator.\n\
+        As far as I know, it's the first one which has been written in Rust!");
 }
